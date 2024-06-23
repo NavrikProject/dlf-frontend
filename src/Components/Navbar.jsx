5878be3c09ebcbbb8c9054341b75a0658f2b4805
Mahesh <b.mahesh311296@gmail.com>
2024-06-23 13:33:56 +0530

    changes in the config

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import './Navbar.css'
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-       <div className="navbar">
-      <div className="navbar-left">
-        <a href="#">Home</a>
-      </div>
-      <div className="center">
-        <div className="logo">Atlas Finance</div>
-      </div>
-      <div className="navbar-right">
-        <a href="#">About</a>
-        <a href="#">Contact Us</a>
-        <a href="#">Logout</a>
+      <div className="navbar">
+        <div className="navbar-left">
+          <a href="#">Home</a>
+        </div>
+        <div className="center">
+          <div className="logo">DLF</div>
+        </div>
+        <div className="navbar-right">
+          <a href="#">About</a>
+          <a href="#">Contact Us</a>
+          <a href="#">Logout</a>
+        </div>
       </div>
     </div>
+  );
+};
 
-
-    </div>
-  )
-}
-
-export default Navbar
+export default Navbar;
